143fa317db7b55d7c8da779855ad65b0b9acb90e
Shivank Awasthi <shivank.awasthi@engineerbabu.in>
2020-01-13 16:36:47 +0530

    update delete added

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module graphQlTest
 
 go 1.13
 
-require github.com/graphql-go/graphql v0.7.8
+require (
+	github.com/gogo/protobuf v1.0.0
+	github.com/golang/protobuf v1.3.2
+	github.com/gorilla/mux v1.7.3
+	github.com/graphql-go/graphql v0.7.8
+	github.com/infobloxopen/atlas-app-toolkit v0.19.1
+	github.com/infobloxopen/protoc-gen-gorm v0.18.0
+	github.com/jinzhu/gorm v1.9.12
+	github.com/lib/pq v1.1.1
+	google.golang.org/genproto v0.0.0-20200108215221-bd8f9a0ef82f
+)
--- graphqltest.go
+++ graphqltest.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/graphql-go/graphql"
 
+	sa "graphQlTest/sa"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/graphql-go/graphql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+	"google.golang.org/genproto/protobuf/field_mask"
 )
 
-type User struct {
-	Id       int    `json:"id" `
-	UserName string `json:"username" `
-	Email    string `json:"email" `
-	Password string `json:"password" `
-}
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "12345"
+	dbname   = "suser"
+)
 
 func executeQuery(query string, schema graphql.Schema) *graphql.Result {
 	result := graphql.Do(graphql.Params{
@@ -26,14 +34,21 @@ func executeQuery(query string, schema graphql.Schema) *graphql.Result {
 }
 
 func main() {
-	var users []User = []User{
-		User{
-			Id:       1,
-			UserName: "sss",
-			Email:    "sss@email",
-			Password: "sss@password",
-		},
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	fmt.Println(psqlInfo)
+	db, err := gorm.Open("postgres", psqlInfo)
+	if err != nil {
+		fmt.Println("errrrror", err)
+		return
 	}
+	defer db.Close()
+	db.CreateTable(&sa.UserORM{})
+	fmt.Println("connected")
+
+	r := mux.NewRouter()
 
 	userType := graphql.NewObject(graphql.ObjectConfig{
 		Name: "user",
@@ -65,6 +80,7 @@ func main() {
 				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 					id, ok := p.Args["id"].(int)
 					if ok {
+						users, _ := sa.DefaultListUser(context.Background(), db)
 
 						for _, user := range users {
 							if int(user.Id) == id {
@@ -80,6 +96,7 @@ func main() {
 				Type: graphql.NewList(userType),
 
 				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					users, _ := sa.DefaultListUser(context.Background(), db)
 					fmt.Println(users)
 					return users, nil
 				},
@@ -91,7 +108,7 @@ func main() {
 	Mutations := graphql.NewObject(graphql.ObjectConfig{
 		Name: "Mutation",
 		Fields: graphql.Fields{
-			"Createuser": &graphql.Field{
+			"createuser": &graphql.Field{
 				Type: userType,
 				Args: graphql.FieldConfigArgument{
 					"id": &graphql.ArgumentConfig{
@@ -108,14 +125,78 @@ func main() {
 					},
 				},
 				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					var user User
-					user.Id = params.Args["id"].(int)
-					user.UserName = params.Args["username"].(string)
+					var user sa.User
+
+					user.Username = params.Args["username"].(string)
 					user.Email = params.Args["email"].(string)
 					user.Password = params.Args["password"].(string)
-					users = append(users, user)
-					fmt.Println(users)
-					return users, nil
+					_, err := sa.DefaultCreateUser(context.Background(), &user, db)
+					if err != nil {
+						panic(err)
+					}
+					fmt.Println(user)
+					return user, nil
+				},
+			},
+
+			"updateuser": &graphql.Field{
+				Type: userType,
+				Args: graphql.FieldConfigArgument{
+					"id": &graphql.ArgumentConfig{
+						Type: graphql.Int,
+					},
+					"username": &graphql.ArgumentConfig{
+						Type: graphql.String,
+					},
+				},
+				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+
+					id, ok := params.Args["id"].(int)
+					if ok {
+
+						users, _ := sa.DefaultListUser(context.Background(), db)
+
+						for _, user := range users {
+							if int(user.Id) == id {
+								fmt.Println(user)
+								user.Username = params.Args["username"].(string)
+								user, err = sa.DefaultPatchUser(context.Background(), user, &field_mask.FieldMask{
+									Paths: []string{"Username"},
+								}, db)
+
+								return user, nil
+							}
+						}
+					}
+					return nil, nil
+
+				},
+			},
+
+			"deleteuser": &graphql.Field{
+				Type: userType,
+				Args: graphql.FieldConfigArgument{
+					"id": &graphql.ArgumentConfig{
+						Type: graphql.Int,
+					},
+				},
+				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+					users, err := sa.DefaultListUser(context.Background(), db)
+					if err != nil {
+						panic(err)
+					}
+					for _, user := range users {
+						if int(user.Id) == params.Args["id"] {
+							fmt.Println(user)
+							err := sa.DefaultDeleteUser(context.Background(), user, db)
+							if err != nil {
+								panic(err)
+							}
+							return user, nil
+						}
+
+					}
+					return nil, err
 				},
 			},
 		},
@@ -128,10 +209,11 @@ func main() {
 	})
 	fmt.Println(schema)
 
-	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(r.URL)
 		result := executeQuery(r.URL.Query().Get("query"), schema)
 		fmt.Println(result)
 	})
-	http.ListenAndServe(":80", nil)
+	fmt.Println(sa.DefaultListUser(context.Background(), db))
+	http.ListenAndServe(":80", r)
 }
